5abd9b1436436c869883c70b96ab85dc9a9a3407
George <kesaev@gmail.com>
2019-03-20 18:38:16 +0200

    my name change

--- GitWelcome/hello.java
+++ GitWelcome/hello.java
@@ -5,6 +5,6 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		
-		System.out.println("My name is: ");
+		System.out.println("My name is <Your Name Here>.");
 	}
 }
