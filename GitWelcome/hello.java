7b075272e66018d79e23357788c7039277728b4b
terkmail <terkmail@gmail.com>
2019-11-27 12:23:46 +0200

    added my name

--- GitWelcome/hello.java
+++ GitWelcome/hello.java
@@ -5,6 +5,6 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		
-		System.out.println("My name is <Your Name Here>.");
+		System.out.println("My name is Terkel.");
 	}
 }
